6dccd278bb41ab68e6987a13ba90c2ee29486282
Darshana Senevirathna <39029875+Darshana97@users.noreply.github.com>
2020-08-30 11:46:44 +0530

    Add project files.

---
+++ methods/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.EvenNumber(20);
+
+
+            
+        }
+
+        public  void EvenNumber(int x)
+        {
+            int Num = 0;
+            while (Num <= x)
+            {
+                Console.WriteLine(Num);
+                Num += 2;
+            }
+            Console.ReadLine();
+        }
+    }
+}
